753192ecf2ec07008f527fd7818a371f5d3e77fa
wadwandelaar <wadwandelaar@gmail.com>
2025-08-14 13:03:39 +0200

    Update all

--- custom_components/toeristenbelasting/__init__.py
+++ custom_components/toeristenbelasting/__init__.py
@@ -1,10 +1,11 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass, entry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Moderne aanroep voor nieuwere HA versies
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
--- custom_components/toeristenbelasting/sensor.py
+++ custom_components/toeristenbelasting/sensor.py
@@ -1,19 +1,22 @@
 from datetime import datetime
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.event import async_track_time_change
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    async_add_entities([TouristTaxSensor(hass, config_entry)])
+async def async_setup_entry(hass, entry, async_add_entities):
+    async_add_entities([TouristTaxSensor(hass, entry)])
 
-class TouristTaxSensor(Entity):
+class TouristTaxSensor(SensorEntity):
+    _attr_name = "Tourist Taxes"
+    _attr_icon = "mdi:cash-euro"
+
     def __init__(self, hass, config_entry):
         self.hass = hass
         self._config = config_entry.data
-        self._state = 0
+        self._attr_native_value = 0
         self._guests = 0
         self._days = {}
         
-        # Start daily updates at 23:00
+        # Start dagelijkse update om 23:00
         async_track_time_change(
             hass, 
             self._update_daily, 
@@ -22,34 +25,20 @@ class TouristTaxSensor(Entity):
         )
 
     @property
-    def name(self):
-        return "Tourist Taxes"
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
     def extra_state_attributes(self):
         return {
             'days': self._days,
             'guests': self._guests,
-            'price_per_person': self._config['price_per_person']
+            'price': self._config['price_per_person']
         }
 
     async def _update_daily(self, now):
-        """Update counts daily at 23:00"""
+        """Dagelijkse update"""
         persons = [
             e for e in self.hass.states.async_entity_ids("person") 
             if self.hass.states.get(e).state == self._config['home_zone']
         ]
         
-        day_name = now.strftime("%A %d %b")
-        self._days[day_name] = len(persons) + self._guests
-        self._state = sum(self._days.values()) * self._config['price_per_person']
+        self._days[now.strftime("%A %d %b")] = len(persons) + self._guests
+        self._attr_native_value = round(sum(self._days.values()) * self._config['price_per_person'], 2)
         self.async_write_ha_state()
-
-    async def update_guests(self, guests):
-        """Public method to update guests"""
-        self._guests = guests
-        await self._update_daily(datetime.now())
