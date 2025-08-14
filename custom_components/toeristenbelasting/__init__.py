6d0f6181c24bbd3bdc57ea90328bcfecbebfca96
wadwandelaar <wadwandelaar@gmail.com>
2025-08-14 14:50:18 +0200

    Update all

--- custom_components/toeristenbelasting/__init__.py
+++ custom_components/toeristenbelasting/__init__.py
@@ -1,11 +1,32 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
+from datetime import datetime
+from homeassistant.core import ServiceCall
+from homeassistant.helpers.typing import HomeAssistantType
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistantType, config: dict):
+    """Set up the component."""
+    async def handle_force_update(call: ServiceCall):
+        """Handle force update service call."""
+        sensor_entity = next(
+            (entity for entity in hass.data.get('sensor', {}).entities 
+            if entity.entity_id == 'sensor.tourist_taxes'),
+            None
+        )
+        if sensor_entity:
+            await sensor_entity._update_daily(datetime.now())
+
+    # Registreer de service
+    hass.services.async_register(
+        DOMAIN,
+        "force_update",
+        handle_force_update
+    )
+    
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Moderne aanroep voor nieuwere HA versies
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+async def async_setup_entry(hass: HomeAssistantType, entry):
+    """Set up from config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    )
     return True
--- custom_components/toeristenbelasting/sensor.py
+++ custom_components/toeristenbelasting/sensor.py
@@ -6,7 +6,6 @@ from homeassistant.helpers.event import async_track_time_change
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the sensor."""
     async_add_entities([TouristTaxSensor(hass, config_entry)])
 
 class TouristTaxSensor(Entity):
@@ -34,36 +33,34 @@ class TouristTaxSensor(Entity):
         }
 
     def _schedule_daily_update(self):
-        """Schedule daily update using input_datetime."""
+        """Plan update op ingestelde tijd"""
         if self._unsub_time:
             self._unsub_time()
 
         time_state = self.hass.states.get("input_datetime.tourist_tax_update_time")
-        if not time_state:
-            _LOGGER.error("Input datetime not found!")
-            return
-
-        hour = int(time_state.attributes['hour'])
-        minute = int(time_state.attributes['minute'])
-
-        self._unsub_time = async_track_time_change(
-            self.hass,
-            self._update_daily,
-            hour=hour,
-            minute=minute
-        )
-        _LOGGER.debug(f"Scheduled daily update at {hour}:{minute}")
+        if time_state:
+            hour = int(time_state.attributes['hour'])
+            minute = int(time_state.attributes['minute'])
+            self._unsub_time = async_track_time_change(
+                self.hass,
+                self._update_daily,
+                hour=hour,
+                minute=minute
+            )
 
     async def _update_daily(self, now):
-        """Execute daily update."""
-        _LOGGER.debug("Running daily update")
-        zone = self._config.get('home_zone', 'zone.home')
+        """Dagelijkse update uitvoeren"""
+        zone = self._config['home_zone']
         persons = [
             e for e in self.hass.states.async_entity_ids("person") 
             if self.hass.states.get(e).state == zone
         ]
-        
         day_name = now.strftime("%A %d %b")
         self._days[day_name] = len(persons)
-        self._state = sum(self._days.values()) * self._config.get('price_per_person', 2.40)
+        self._state = sum(self._days.values()) * self._config['price_per_person']
         self.async_write_ha_state()
+
+    async def async_update_guests(self, guests):
+        """Update aantal logés (voor service call)"""
+        self._guests = guests
+        await self._update_daily(datetime.now())
