2a6333de7a909f73e48fa5f3808558a518114969
wadwandelaar <wadwandelaar@gmail.com>
2025-08-14 14:42:12 +0200

    Update all

--- custom_components/toeristenbelasting/sensor.py
+++ custom_components/toeristenbelasting/sensor.py
@@ -1,59 +1,69 @@
 from datetime import datetime
-from homeassistant.components.sensor import SensorEntity
+import logging
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_time_change
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([TouristTaxSensor(hass, entry)])
+_LOGGER = logging.getLogger(__name__)
 
-class TouristTaxSensor(SensorEntity):
-    _attr_name = "Tourist Taxes"
-    _attr_icon = "mdi:cash-euro"
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the sensor."""
+    async_add_entities([TouristTaxSensor(hass, config_entry)])
 
+class TouristTaxSensor(Entity):
     def __init__(self, hass, config_entry):
         self.hass = hass
         self._config = config_entry.data
-        self._attr_native_value = 0
-        self._guests = 0
+        self._state = 0
         self._days = {}
-        
-        # Start dagelijkse update om 23:00
-        async_track_time_change(
-            hass, 
-            self._update_daily, 
-            hour=23, 
-            minute=0
-        )
+        self._unsub_time = None
+        self._schedule_daily_update()
+
+    @property
+    def name(self):
+        return "Tourist Taxes"
+
+    @property
+    def state(self):
+        return self._state
 
     @property
     def extra_state_attributes(self):
         return {
             'days': self._days,
-            'guests': self._guests,
-            'price': self._config['price_per_person']
+            'update_time': self.hass.states.get("input_datetime.tourist_tax_update_time").state
         }
 
-    async def _update_daily(self, now):
-        """Dagelijkse update"""
-        persons = [
-            e for e in self.hass.states.async_entity_ids("person") 
-            if self.hass.states.get(e).state == self._config['home_zone']
-        ]
-        
-        self._days[now.strftime("%A %d %b")] = len(persons) + self._guests
-        self._attr_native_value = round(sum(self._days.values()) * self._config['price_per_person'], 2)
-        self.async_write_ha_state()
+    def _schedule_daily_update(self):
+        """Schedule daily update using input_datetime."""
+        if self._unsub_time:
+            self._unsub_time()
 
-    async def _schedule_daily_update(self):
-        """Lees tijd uit input_datetime"""
-        time_str = self.hass.states.get("input_datetime.tourist_tax_update_time").state
-        hour, minute, _ = map(int, time_str.split(":"))
-        
+        time_state = self.hass.states.get("input_datetime.tourist_tax_update_time")
+        if not time_state:
+            _LOGGER.error("Input datetime not found!")
+            return
+
+        hour = int(time_state.attributes['hour'])
+        minute = int(time_state.attributes['minute'])
+
         self._unsub_time = async_track_time_change(
             self.hass,
             self._update_daily,
             hour=hour,
             minute=minute
         )
+        _LOGGER.debug(f"Scheduled daily update at {hour}:{minute}")
+
+    async def _update_daily(self, now):
+        """Execute daily update."""
+        _LOGGER.debug("Running daily update")
+        zone = self._config.get('home_zone', 'zone.home')
+        persons = [
+            e for e in self.hass.states.async_entity_ids("person") 
+            if self.hass.states.get(e).state == zone
+        ]
         
-        # Update attributen
-        self._attr_extra_state_attributes["update_time"] = f"{hour:02d}:{minute:02d}"
+        day_name = now.strftime("%A %d %b")
+        self._days[day_name] = len(persons)
+        self._state = sum(self._days.values()) * self._config.get('price_per_person', 2.40)
+        self.async_write_ha_state()
