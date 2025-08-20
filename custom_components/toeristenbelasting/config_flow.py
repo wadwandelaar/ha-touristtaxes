8c9bba00b071b3424bbc3d9d0233b642e918810a
wadwandelaar <wadwandelaar@gmail.com>
2025-08-20 15:43:07 +0200

    Update config flow

--- custom_components/toeristenbelasting/config_flow.py
+++ custom_components/toeristenbelasting/config_flow.py
@@ -1,10 +1,12 @@
 from homeassistant import config_entries
 import voluptuous as vol
-from .const import DOMAIN, DEFAULT_PRICE
+from .const import DOMAIN, DEFAULT_PRICE, DEFAULT_ZONE
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is not None:
+            # Voeg de vaste zone toe aan de configuratie
+            user_input["zone"] = DEFAULT_ZONE  # "camping"
             return self.async_create_entry(title="Tourist Taxes", data=user_input)
 
         return self.async_show_form(
